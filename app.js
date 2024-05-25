f06071e7d10f092d528e457c9ec4723ab8d11cbf
RiffatRafi <157373052+RiffatRafi@users.noreply.github.com>
2024-05-25 23:52:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,418 @@
+
+//chapter 1
+
+// let welcome = "Welcome to our website! We're glad you're here."
+
+
+// alert(welcome)
+
+// let error = 'Error! Please Enter a valid Password'
+
+// alert(error)
+
+
+
+// let hellow = 'Welcome to JS Land...'
+// let hi = 'Happy Coding!'
+
+// alert(hellow + "\n" + hi)
+
+// let first = 'Welcome to JS Land...'
+// let second = 'Happy Coding!'
+
+// alert (first)
+// alert (second)
+
+
+//chapter 2
+
+// var userName = 'hi'
+
+// var yourName = 'Riffat Rafi'
+
+// let message = 'Hello World'
+// alert (message)
+
+// Define variables to store student's bio data
+let studentName = 'Johne Doe'
+let studentAge = '15 years old'
+let course = 'Certified Mobile Application Development'
+
+// Display the student's bio data using alert boxes
+// alert (studentName)
+// alert (studentAge)
+// alert (course)
+
+// let food = 'PIZZA'
+// alert (food)
+// food = food.slice(0,-1)
+// alert (food)
+// food = food.slice(0,-1)
+// alert (food)
+// food = food.slice(0,-1)
+// alert (food)
+// food = food.slice(0,-1)
+// alert (food)
+// food = food.slice(0,-1)
+// alert (food)
+// food = food.slice(0,-1) 
+
+// let email =  'riffatrafi756@gmail.com'
+// alert( 'My email adress is:' + email)
+
+// let book = 'A Smarter Way to learn JavaScript'
+// alert (book)
+
+// let content = 'Yah! I can write HTML content through Javascript'
+
+// document.write (content)
+
+// let end = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+// alert (end)
+
+//chap np 3
+
+// let age = 'I am 20 year old'
+// alert(age );
+
+// let birthYear = "<br>"+'My bith year is 2003'
+// let data = 'Datatype of my declared variable is number'
+// document.write(birthYear + "<br>" + data)
+
+
+// let  visitorName = '+ "<br>" <h2>John Doe ordered 5 T-shirt(s) on XYZ Clothing store<h2>'
+
+// document.write (visitorName)
+
+//chapter 4
+
+
+let name1, name2, name3;
+
+// legal names
+
+let yourName, yourAge, contactNumber, userInput, words;
+
+// Illegal names
+
+// let 123history, ^book, var ,*Store ,@user ;
+
+// let heading = '<h1>Rules for Naming JS <h1>'
+
+// let rule1 = "Variable names can only contain numner.$ and _ "
+// let rule2 = "Variables must begin with aletter, $ or _ . For example $name, _name or Name"
+// let rule3 = "Variable names are case sensitive"
+// let rule4 = "Variable names not be JS keyword"
+
+//  document.write(heading +"<br>"+ rule1 +"<br>"+rule2 +"<br>"+rule3 +"<br>"+rule4 )
+
+//chapter no 5
+
+//task 1
+
+// let num1 = +prompt('write num1')
+// let num2 = +prompt('write num2')
+
+// let result = num1 + num2
+// document.write("<br>" + 'The Sum is ' + result)
+
+// let num3 = prompt('write num3')
+// let num4 = prompt('write num4')
+
+// let answer = num3 * num4
+// document.write("<br>" + 'The multiplication of these numbers is ' + answer)
+
+// let num5 = prompt('write num5')
+// let num6 = prompt('write num6')
+
+// let divide = num5 / num6
+// document.write("<br>" + 'On dividing these numbers we get' + divide)
+
+// let num7 = prompt('write num7')
+// let num8 = prompt('write num8')
+
+// let results = num1 % num2
+// document.write("<br>" + 'The modulus is ' + results)
+
+
+// let num9 = prompt('write num9')
+// let num10 = prompt('write num10')
+
+// let sub = num1 - num2
+// document.write("<br>" + 'The subtraction value is ' + sub)
+
+//task 3
+
+// a. Variable declare karna
+// var num;
+
+// b. Variable ki value browser mein dikhana
+// document.write("Value after variable declaration is: " + num + "<br>");
+
+// // c. Variable ko kisi number se initialize karna
+// num = 5;
+
+// d. Variable ki value browser mein dikhana
+// document.write("Initial value: " + num + "<br>");
+
+// e. Variable ko increment karna
+// num++;
+
+// f. Variable ki value browser mein dikhana
+// document.write("Value after increment is: " + num + "<br>");
+
+// g. Variable mein 7 add karna
+// num = num + 7;
+
+// h. Variable ki value browser mein dikhana
+// document.write("Value after addition is: " + num + "<br>");
+
+// i. Variable ko decrement karna
+// num--;
+
+// j. Variable ki value browser mein dikhana
+// document.write("Value after decrement is: " + num + "<br>");
+
+// // k. Variable ki value ko 3 se divide karne ke baad remainder dikhana
+// var remainder = num % 3;
+// document.write("The remainder is: " + remainder);
+
+
+// //task 4
+
+// // Store the ticket price in a variable
+// // const ticketPrice = 600;
+
+// // Calculate the cost of buying 5 tickets
+// // const totalCost = ticketPrice * 5;
+
+// // Print the total cost
+// // document.write("Total cost of buying 5 tickets:", totalCost, "PKR");
+
+//task 6
+
+// a. Celsius temperature ko ek variable mein store karna
+// var celsiusTemperature = 25;
+
+// b. Celsius ko Fahrenheit mein convert karna aur output karna
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F <br>");
+
+// c. Fahrenheit temperature ko ek variable mein store karna
+// var fahrenheitTemperature2 = 77;
+
+// d. Fahrenheit ko Celsius mein convert karna aur output karna
+// var celsiusTemperature2 = (fahrenheitTemperature2 - 32) * 5/9;
+// document.write(fahrenheitTemperature2 + "°F is " + celsiusTemperature2 + "°C");
+ 
+//task 7
+
+// var price1 = 650;
+// document.write("Price of Item 1 :" + price1 + "<br>")
+// var price2 = 100;
+// document.write("Price of Item 2 :" + price2 + "<br>")
+// var quantity1 = 3;
+// document.write("Quantity of item 1 :" +quantity1+ "<br>"  )
+// var quantity2 = 7;
+// document.write("Quantity of item 2 :"+ quantity2  + "<br>")
+// var shippingCharges = 100;
+// document.write("Shipping Charges :" + shippingCharges + "<br>" )
+
+// var res =  price1 * quantity1 + price2 * quantity2 + shippingCharges;
+
+// document.write("Total Cost of Your Order Is :"+ " " +res)
+
+//task 8
+
+// document.write( "Mark Sheet" + "<br>")
+
+// var totalMarks = 980;
+// document.write( "TotalMarks :" +" "+ totalMarks + "<br>")
+// var obtainedMarks = 804;
+// document.write( "ObtainedMarks :" +" "+ obtainedMarks + "<br>")
+
+// var per = obtainedMarks / totalMarks * 100;
+// document.write( "Percentage :" +" "+ per)
+
+
+//task 9
+
+// var totalRupees = (10 * 104.80) + (25 * 28);
+// document.write( "Total currency in Pakistani Rupees: " + totalRupees);
+
+//task 10
+
+// var num = 2;
+
+// var res = ((num + 5) * 10 ) / 2;
+// document.write( "Result : "+ res)
+
+//task 11
+
+// let currentYear = 2016;
+// let birthYear = 1992;
+// document.write( "Current Year :" + currentYear +"<br>"+" Birth Year :" + birthYear+ "<br>")
+// let age1 =  currentYear - birthYear
+// let age2 = age1 - 1
+// document.write("They are either"+" ", age1+" " ,"or"+" ", age2 , "years old.")
+
+//task 12
+// const pi = 3.142
+// let radius = 20;
+// var cir = 2 * pi * radius;
+// var area = pi * radius * radius;
+
+// document.write( "Radius of a Circle is :"+ radius +"<br>"+"The circumference is :" + cir + "<br>")
+// document.write("The Area is :" + area +"<br>" )
+
+
+//task 13
+
+// var dairymilk;
+// var age = 20;
+// var maxAge = 50;
+// var amount = 1;
+
+// document.write("favorite snack:" + 'dairymilk' + "<br>")
+// document.write("current age:" + 20 + "<br>")
+// document.write("maximum age:"+ 50 + "<br>")
+// document.write("Amount of snacks per day:" + 1 + "<br>")
+
+// var remaining = maxAge - age;
+// var total = remaining * 365 * amount;
+// document.write( "You would eat " + total + " " + 'dairymilk' + " for the rest of your life.");
+
+//Chap 6 - 9
+
+//task 1
+
+// let a = 10 ;
+// document.write("The value of a is : " + a + "<br>"+ "<br>")
+// document.write("the value of ++a is : " + ++a + "<br>" + "Now the value of a is : " + a +"<br>" +"<br>")
+// document.write("the value of a++ is : " + a++ + "<br>" + "Now the value of a is : " + a +"<br>" +"<br>")
+// document.write("the value of --a is : " + --a + "<br>" + "Now the value of a is : " + a +"<br>" +"<br>")
+// document.write("the value of a-- is : " + a-- + "<br>" + "Now the value of a is : " + a +"<br>" +"<br>")
+
+//task 2
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+
+// document.write("a = ", a + "<br>"); 
+// document.write("b = ", b + "<br>"); 
+// document.write("Result is : = ", result); 
+
+//task 3
+
+// Ask user for a number
+// var userinput = prompt("Enter a number to display its multiplication table:");
+
+// // Convert user input to a number
+// var number = parseInt(userInput);
+
+// If user input is not a valid number, default to 5
+// if (isNaN(number)) {
+//     number = 5;
+//     document.write("<p>No valid number entered. Defaulting to 5.</p>");
+// } else {
+//     document.write("<p>Multiplication table for " + number + ":</p>");
+// }
+
+// // Display multiplication table
+// document.write("<ul>");
+// for (var i = 1; i <= 10; i++) {
+//     document.write("<li>" + number + " x " + i + " = " + (number * i) + "</li>");
+// }
+// document.write("</ul>");
+
+//task 5
+
+var sub1 = prompt("Enter your Subject elective 1 ")
+var sub2 = prompt("Enter your Subject elective 1 ")
+var sub3 = prompt("Enter your Subject elective 1 ")
+
+var total = 100
+finalTotal = 300;
+
+var obt1 = +prompt("Enter your obtained marks in your 1st elective subject")
+var obt2 = +prompt("Enter your obtained marks in your 2nd elective subject")
+var obt3 = +prompt("Enter your obtained marks in your 3rd elective subject")
+
+var paper1 = obt1 / total * 100;
+var paper2 = obt2 / total * 100;
+var paper3 = obt3 / total * 100;
+
+var obtAll = obt1+obt2+obt3;
+var finalPer =  +obt1+obt2+obt3 / finalTotal *100;
+
+
+document.write(
+
+          "<table>"+
+          "<tr>"+
+          "<th>"+"subject"+"</th>"
+          +"<th>"+"Total Marks"+"</th>"
+          +"<th>"+"Obtained Marks"+"</th>"
+          +"<th>"+"Percentage"+"</th>"
+          +"</tr>"+
+              "<tr>"+
+              "<td>"+sub1+"</td>"
+              +"<td>"+total+" "+"</td>"
+              +"<td>"+obt1+" "+"</td>"
+              +"<td>"+paper1+"</td>"
+              +"</tr>"
+
+          + "</table>"
+
+)
+
+document.write(
+
+    "<table >"+
+    "<tr>"+
+    "<th>"+"</th>"
+    +"</tr>"+
+        "<tr>"+
+        "<td>"+sub2+"</td>"
+       +"<td>"+total+" "+"</td>"
+       +"<td>"+obt2+" "+"</td>"
+       +"<td>"+paper2+"</td>"
+        +"</tr>"
+
+    + "</table>"
+
+)
+
+document.write(
+
+    "<table >"+
+    "<tr>"+
+    "<th>"+"</th>"
+    +"</tr>"+
+        "<tr>"+
+        "<td>"+sub3+"</td>"
+        +"<td>"+total+" "+"</td>"
+        +"<td>"+obt3+" "+"</td>"
+        +"<td>"+paper3+"</td>"
+        +"</tr>"
+
+    + "</table>"
+
+)
+
+document.write(
+
+    "<table >"+
+    "<tr>"+
+    "<th>"+"</th>"
+    +"</tr>"+
+        "<tr>"+
+        "<td>"+finalTotal+" "+"</td>"
+        +"<td>"+obtAll+" "+"</td>"
+        +"<td>"+finalPer+" "+"</td>"
+        +"</tr>"
+
+    + "</table>"
+
+)
+
